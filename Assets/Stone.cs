fae69c2b6724d90800aa6e2110e0fc553e96da25
menyawa <41637976+menyawa@users.noreply.github.com>
2019-01-17 15:49:45 +0900

    first commit

---
+++ Assets/Stone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Stone : MonoBehaviour
+{
+	[SerializeField] private GameObject stone;
+	private Transform gridTransform;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		gridTransform = this.gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ClickGrid()
+	{
+		Debug.Log("test");
+		Vector3 gridPosition = gridTransform.position;
+		Instantiate(stone, new Vector3(gridPosition.x, gridPosition.y + (float)0.1, gridPosition.z), Quaternion.identity);
+	}
+}
